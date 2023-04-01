0c79b890b72a95b7e401d8933f403fb16c8c3429
username <mohseen_abasi@yahoo.com>
2023-04-02 00:11:25 +0330

    add colors npm and connect to mongodb atlas

--- chat app/server/index.js
+++ chat app/server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotEnv = require("dotenv");
 const cors = require("cors");
+const ColorsLog = require("colors");
+const connectToMongoDb = require("./config/dbConnect");
 const app = express();
 const data = require("./data");
 app.use(cors());
 console.log(data);
 dotEnv.config();
+//* data base connection
+connectToMongoDb();
 
+//* test Routes
 app.get("/", (req, res) => {
   return res.status(200).send(data);
 });
@@ -21,5 +26,5 @@ app.get("/:id", (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`server is run at ${port}`);
+  console.log(`server is run at ${port}`.brightGreen.bold);
 });
