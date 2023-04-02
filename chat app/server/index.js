68bb4f267ca68716c3c3d8aca57b33d928a83704
username <mohseen_abasi@yahoo.com>
2023-04-02 17:03:41 +0330

    add middleware for control notFound routes and handler Error

--- chat app/server/index.js
+++ chat app/server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotEnv = require("dotenv");
 const cors = require("cors");
-const ColorsLog = require("colors");
+const colors = require("colors");
 const connectToMongoDb = require("./config/dbConnect");
 const app = express();
 const data = require("./data");
+
+const notFoundRoutes = require("./middleware/controlRoutes");
+const errorHandler = require("./middleware/errorHandel");
+
 const router = require("./routes/user");
 
 app.use(cors());
@@ -18,12 +22,13 @@ app.use(express.json());
 //*routes
 app.use("/", router);
 
+//*send data from data.js to localhost
 app.get("/", (req, res) => {
   return res.status(200).send(data);
 });
-
-
-
+//*middleware control route & handlerError
+app.use(errorHandler);
+app.use(notFoundRoutes);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
