f47f0cf05d3cefc1a376b6db1b149ff74d874aba
username <mohseen_abasi@yahoo.com>
2023-04-09 10:39:13 +0330

    add chat routes add gruop ,remove group , add chat group and so on

--- chat app/server/controller/chat.js
+++ chat app/server/controller/chat.js
@@ -63,4 +63,102 @@ const usersChat = async (req, res) => {
   } catch (err) {}
 };
 
-module.exports = { usersChat, accessChat };
+const createGroupChat = async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res
+      .status(400)
+      .send({ message: "please Fill all the feilds" });
+  }
+  var users = JSON.parse(req.body.users);
+  if (users.length < 2) {
+    return res.status(400).send("more than 2 users for group chat");
+  }
+  users.push(req.user);
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+    const fullGropchat = await Chat.findOne({
+      _id: groupChat._id,
+    })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    return res.status(200).json(fullGropchat);
+  } catch (err) {
+    return res.status(400).send(err.message);
+  }
+};
+
+const renameGroup = async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updateChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updateChat) {
+    return res.status(400).send("chat not Found");
+  } else {
+    return res.json(updateChat);
+  }
+};
+const addToGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+  const remove = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("users", "-confirmPassword ");
+  if (!remove) {
+    return res.status(400).send("chat not found");
+  } else {
+    return res.json(remove);
+  }
+};
+
+const removeFromGroup = async (req, res) => {
+  const { chatId, userId } = req.body;
+  const remove = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+    .populate("users", "-confirmPassword ");
+  if (!remove) {
+    return res.status(400).send("chat not found");
+  } else {
+    return res.json(remove);
+  }
+};
+module.exports = {
+  usersChat,
+  accessChat,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+};
