72e979837200b6ddb793371a2f0300d14ee31b82
username <mohseen_abasi@yahoo.com>
2023-04-15 11:43:00 +0330

    init tailwind css & add config - colors

---
+++ chat_app/client/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div >
+  <h1 class="bg-success">
+    Hello world!
+  </h1>
+  <h1 className='dark:text-slate-400'>
+    hehehe
+  </h1>
+    </div>
+  )
+}
+
+
+export default App
---
+++ chat_app/client/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/*.jsx"
+  ],
+  theme: {
+    colors:{
+      'midnight': '#121063',
+      'success':"#6ee7b7",
+      "warning":"#fdba74",
+      "info":"#3b82f6",
+      "btn":"#0284c7",
+      "btn-hover":"#0369a1",
+      "border":"#e0e7ff",
+    "boxshadow":"#e5e5e5",
+    "textcolor":"#f0f9ff",
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
