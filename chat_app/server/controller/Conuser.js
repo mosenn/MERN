601636a9278f26f316263c17ec91eef99cc59d6f
username <mohseen_abasi@yahoo.com>
2023-04-15 18:58:09 +0330

    set jwt and send token with cookie have response at network tab is worked

--- chat_app/server/controller/Conuser.js
+++ chat_app/server/controller/Conuser.js
@@ -1,16 +1,24 @@
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const registerUser = async (req, res) => {
+  const { username, password } = req.body;
+  console.log("username:", username, "password:", password);
+  console.log(res.coockie, "coockie");
+  try {
+    const createUser = await User.create({ username, password });
+    jwt.sign({ userId: createUser._id }, "secret", {}, (err, token) => {
+      if (err) throw err;
+      return res
+        .cookie("token", token)
+        .status(201)
+        .json({ id: createUser._id, username: createUser.username });
+    });
 
-const User = require('../model/User')
-const registerUser = async (req , res) => {
-    const {username,  password} = req.body
-    console.log("username:", username , "password:" ,password)
-    try {
-         const createUser = await User.create({username , password})
-        return res.status(201).json(createUser)
-    }catch(err) {
-        console.log(err)
+    // return res.status(201).json(createUser);
+  } catch (err) {
+    // return res.status(500).json(err, "jwt have proplem");
+    return res.status(500).json({ err: err, message: "jwt have proplem" });
+  }
+};
 
-    }
-
-}
-
-module.exports = {registerUser}
+module.exports = { registerUser };
