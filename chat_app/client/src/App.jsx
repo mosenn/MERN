4dbe879d78b847ba577bcb9a1c06443439fb32dd
username <mohseen_abasi@yahoo.com>
2023-04-16 10:32:38 +0330

    inital  state user & userId in context they work

--- chat_app/client/src/App.jsx
+++ chat_app/client/src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { Register } from "./pages/Register";
 import { useGlobalcontext } from "./Context/Context";
 function App() {
-  const { name } = useGlobalcontext();
+  const { user, userId } = useGlobalcontext();
   return (
     <div className="bg-red">
-      <h1 className="text-lg bg-blue-400">{name}</h1>
+      <h1 className="text-lg bg-blue-400">{user}</h1>
+      <h1 className="text-lg bg-blue-400">{userId}</h1>
       <Register />
     </div>
   );
--- chat_app/client/src/Context/Context.jsx
+++ chat_app/client/src/Context/Context.jsx
@@ -2,8 +2,10 @@ import { useContext, createContext, useState } from "react";
 
 const AppContext = createContext();
 const AppProvider = ({ children }) => {
+  const [user, setUser] = useState('mohsen ab');
+  const [userId, setUserId] = useState(1);
   return (
-    <AppContext.Provider value={{ name: "mohsen" }}>
+    <AppContext.Provider value={{ user, setUser, userId, setUserId }}>
       {children}
     </AppContext.Provider>
   );
