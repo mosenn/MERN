af6dee0090f2f0e4e0cab90d4d252938bfe0515e
username <mohseen_abasi@yahoo.com>
2023-04-17 18:59:01 +0330

    add listner for get message from ws

--- chat_app/client/src/pages/Chat.jsx
+++ chat_app/client/src/pages/Chat.jsx
@@ -1,8 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Chat = () => {
+  const [ws, setWs] = useState();
+  const handleMessage = (e) => {
+    console.log("socket message : ", e.data);
+  };
   useEffect(() => {
-    new WebSocket("ws://localhost:4010");
+    const socket = new WebSocket("ws://localhost:4010");
+    setWs(socket);
+    socket.addEventListener("message", handleMessage);
   }, []);
+
   return <div>chat</div>;
 };
