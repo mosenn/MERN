08da8f7f1e3938e28455fff70fc12a3f6254675f
username <mohseen_abasi@yahoo.com>
2023-05-02 14:54:42 +0330

    have token with click on btn

--- login system/client/src/component/Login.jsx
+++ login system/client/src/component/Login.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
   const handleGithubLogin = async () => {
     const param = new URLSearchParams(window.location.search);
@@ -12,17 +13,27 @@ const Login = () => {
     // console.log(data, "data");
     // const { token } = data;
     // console.log(token);
+    if (code && localStorage.getItem("accessToken") === null) {
+      const getAccessToken = async () => {
+        const data = await axios.get(
+          `http://localhost:3000/getAccessToken?code=${code}`
+        );
+        console.log("data in if :", data);
+
+        return data;
+      };
+      getAccessToken();
+    }
   };
 
-  useEffect(() => {
-    handleGithubLogin();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div>
       <Link to="https://github.com/login/oauth/authorize?client_id=373605aaa0df2e4fecde">
         <h1>Login with Github</h1>
       </Link>
+      <button onClick={handleGithubLogin}>get token</button>
     </div>
   );
 };
