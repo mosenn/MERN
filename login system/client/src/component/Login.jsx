ba72ac0a622ac9819bb4968a922018a2718252f3
username <mohseen_abasi@yahoo.com>
2023-05-02 18:03:24 +0330

    now have 3 btn have token and userdata in the clinet at console.log

--- login system/client/src/component/Login.jsx
+++ login system/client/src/component/Login.jsx
@@ -1,39 +1,86 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 const Login = () => {
+  const [token, setToken] = useState("");
+
+  // const handleGithubLogin = async () => {
+  //   const param = new URLSearchParams(window.location.search);
+  //   const code = param.get("code");
+  //   console.log(code);
+
+  //   // window.location.href =
+  //   // "https://github.com/login/oauth/authorize?client_id=373605aaa0df2e4fecde";
+  //   // const response = await fetch("http://localhost:3000/login/github/callback");
+  //   // const data = await response.json();
+  //   // console.log(data, "data");
+  //   // const { token } = data;
+  //   // console.log(token);
+
+  //   if (code && localStorage.getItem("accessToken") === null) {
+  //     const data = await axios.get(
+  //       `http://localhost:3000/getAccessToken?code=${code}`
+  //     );
+
+  //     setToken(data.data.token);
+  //     console.log("data in if :", data);
+
+  //     return data;
+  //     // const getAccessToken = async () => {};
+  //     // getAccessToken();
+  //   }
+  // };
   const handleGithubLogin = async () => {
+    // Redirect user to GitHub login page
+    window.location.href =
+      "https://github.com/login/oauth/authorize?client_id=373605aaa0df2e4fecde";
+
+    // Get the code from the query parameters
     const param = new URLSearchParams(window.location.search);
     const code = param.get("code");
     console.log(code);
-    // window.location.href =
-    //   "https://github.com/login/oauth/authorize?client_id=373605aaa0df2e4fecde";
-    // const response = await fetch("http://localhost:3000/login/github/callback");
-    // const data = await response.json();
-    // console.log(data, "data");
-    // const { token } = data;
-    // console.log(token);
-    if (code && localStorage.getItem("accessToken") === null) {
-      const getAccessToken = async () => {
-        const data = await axios.get(
-          `http://localhost:3000/getAccessToken?code=${code}`
-        );
-        console.log("data in if :", data);
 
-        return data;
-      };
-      getAccessToken();
+    // Get the access token using the code
+    if (code && !token) {
+      const response = await axios.get(
+        `http://localhost:3000/getAccessToken?code=${code}`
+      );
+      const token = response.data.token;
+      console.log("token in handleGithubLogin :", token);
+
+      // Store the token in state
+      setToken(token);
     }
   };
 
-  useEffect(() => {}, []);
-
+  const getUserdata = async () => {
+    console.log(token, "in getuserdata");
+    const userData = await axios.get("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${token}`, // add 'Bearer' before token
+        // Authorization: req.get("Authorization"), // add 'Bearer' before token
+      },
+    });
+    console.log("userdata", userData);
+  };
+  // useEffect(() => {
+  //   handleGithubLogin();
+  // }, [checkClick]);
+  console.log(token, "token out side after useeffect");
   return (
     <div>
-      <Link to="https://github.com/login/oauth/authorize?client_id=373605aaa0df2e4fecde">
-        <h1>Login with Github</h1>
-      </Link>
-      <button onClick={handleGithubLogin}>get token</button>
+      {/* <Link to="https://github.com/login/oauth/authorize?client_id=373605aaa0df2e4fecde">
+        Login with github
+      </Link> */}
+      <button onClick={handleGithubLogin}>login with github btn</button>
+      <button onClick={getUserdata}>useerdata</button>
+      {/* {token ? (
+        <div>
+          <h1>USER LOGIN</h1>
+        </div>
+      ) : (
+        <div></div>
+      )} */}
     </div>
   );
 };
