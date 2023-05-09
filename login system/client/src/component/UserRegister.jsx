32673d14463f5da40e994dbd80463e7c32fe1811
username <mohseen_abasi@yahoo.com>
2023-05-09 13:22:17 +0330

    get inputs values registeruser before send to api

--- login system/client/src/component/UserRegister.jsx
+++ login system/client/src/component/UserRegister.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
+
 const UserRegister = () => {
+  const [registerValue, setRegisterValue] = useState({
+    email: "",
+    password: "",
+    confrimPassword: "",
+  });
+  const onchangeHandleRegister = (inputs) => {
+    setRegisterValue({
+      ...registerValue,
+      [inputs.target.name]: inputs.target.value,
+    });
+  };
+  const handleSubmit = (form) => {
+    form.preventDefault();
+    console.log(registerValue);
+  };
   return (
     <div>
-      <form action="">
-        <input type="text" name="email" placeholder="email" />
-        <input type="text" name="password" placeholder="password" />
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          value={registerValue.email}
+          onChange={onchangeHandleRegister}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          value={registerValue.password}
+          onChange={onchangeHandleRegister}
+        />
         <input
           type="text"
           name="confrimPassword"
           placeholder="confrim password"
+          value={registerValue.confrimPassword}
+          onChange={onchangeHandleRegister}
         />
         <button type="submit">Register</button>
       </form>
