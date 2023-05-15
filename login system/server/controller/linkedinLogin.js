cae541b9f6a35497dd8dcd6a30809d9ea710e483
username <mohseen_abasi@yahoo.com>
2023-05-15 08:44:35 +0330

    login with linkedin done

--- login system/server/controller/linkedinLogin.js
+++ login system/server/controller/linkedinLogin.js
@@ -3,8 +3,8 @@ const accessTokenLinkedin = async (req, res) => {
   try {
     const code = req.query.code;
     // console.log("code in controller linkedin", code);
-    const linkedinToken = await getTokenAccessLinkedin(code);
-    return res.status(200).json({linkedinToken});
+    const data = await getTokenAccessLinkedin(code);
+    return res.status(200).json({data});
   } catch (err) {
     console.log(err , 'err in controller');
     return res.status(400).send(err);
