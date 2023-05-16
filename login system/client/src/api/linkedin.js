e169c2377894b0f3aa71ddd60ff82ee5e10b4594
username <mohseen_abasi@yahoo.com>
2023-05-16 06:17:54 +0330

    edit linkedinAcess component and api

--- login system/client/src/api/linkedin.js
+++ login system/client/src/api/linkedin.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+
+//*take url params code pass to api for take token
 export const takeCodeAndPostToServerLinkedin = async (code) => {
   // console.log(code, "code in api clinet"); //* have log code
   try {
@@ -7,60 +9,9 @@ export const takeCodeAndPostToServerLinkedin = async (code) => {
     );
     const data = await response.data;
     // console.log(linkedinData, "linkedin response at _api.js"); //*have log here
+
     return data;
   } catch (err) {
     console.log(err.data, "error in api clinet");
-  }
-};
-
-// export const getUserDataLinkedin = async (token) => {
-//   try {
-//     const data = await fetch("https://api.linkedin.com/v2", {
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`, // add 'Bearer' before token
-//       },
-//     });
-//     const response = await data.json();
-//     console.log(response, 'response in getUserDataLinkedin with fetch"');
-//     // console.log(token, "token in getUserDataLinkedin");
-//     // const localtoken = localStorage.getItem("tokens");
-//     // console.log(localtoken, "localtoken in getUserDataLinkedin");
-//     // const userData = await axios.get(
-//     //   "https://api.linkedin.com/v2/emailAddress",
-//     //   {
-//     //     headers: {
-//     //       "Content-Type": "application/json",
-//     //       Authorization: `Bearer ${token}`, // add 'Bearer' before token
-//     //     },
-//     //   }
-//     // );
-//     // console.log(userData, "in getUserDataLinkedin");
-//     // return userData;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-export const getUserDataLinkedin = async () => {
-  try {
-    const token = localStorage.get("tokens");
-    console.log(token);
-    const axiosgetUserdata = await axios.get("https://api.linkedin.com/v2/me", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log(axiosgetUserdata, "data in getUserDataLinkedin with axios");
-    const data = await fetch("https://api.linkedin.com/v2/me", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const response = await data.json();
-    console.log(response, 'response in getUserDataLinkedin with fetch"');
-    return response;
-  } catch (err) {
-    console.log(err);
   }
 };
--- login system/client/src/component/LinkedinAccess.jsx
+++ login system/client/src/component/LinkedinAccess.jsx
@@ -1,33 +1,18 @@
 import { useEffect } from "react";
-import {
-  takeCodeAndPostToServerLinkedin,
-  getUserDataLinkedin,
-} from "../api/linkedin";
-// import { useNavigate } from "react-router-dom";
+import { takeCodeAndPostToServerLinkedin } from "../api/linkedin";
 
 const LinkedinAccess = () => {
-  // const navigate = useNavigate();
-
   const getLinkedinCodeinParamas = async () => {
     const param = new URLSearchParams(window.location.search);
     const code = param.get("code");
     const data = await takeCodeAndPostToServerLinkedin(code);
-    console.log(data, "data");
-    const userData = await getUserDataLinkedin(data.data?.token);
-    console.log(userData, "userData");
-
-    if (data.data?.token) {
-      localStorage.setItem("tokens", data.data.token);
-      const token = localStorage.getItem("tokens");
-      console.log("token in local linkedin", token);
-
-      // navigate("/profile");
-    }
+    console.log(data);
   };
-  getLinkedinCodeinParamas();
-  // useEffect(() => {
 
-  // });
+  useEffect(() => {
+    getLinkedinCodeinParamas();
+  }, []);
+
   return (
     <div>
       <h1>access code paramas linkedin</h1>
