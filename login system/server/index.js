8c86126bfd9e26bcafb30624e2123eeaa80569cb
username <mohseen_abasi@yahoo.com>
2023-05-21 16:10:32 +0330

    edit connection agin index.js

--- login system/server/index.js
+++ login system/server/index.js
@@ -21,32 +21,20 @@ app.use(cors());
 
 // mongoose.set("strictQuery", false);
 
-// const connecetToDb = async () => {
-//   try {
-//     const connect = await mongoose.connect(process.env.DB_URL);
+const connecetToDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.DB_URL);
 
-//     console.log(`db is connect at ${connect.connection.host}`);
-//   } catch (err) {
-//     console.log(err.message, "data base cant connect");
-//     process.exit(1);
-//   }
-// };
-
-mongoose.set("strictQuery", false);
-//*test
-mongoose
-  .connect("mongodb+srv://mnazgul:qse8303893@cluster0.zutazhf.mongodb.net/loginSystem")
-  .then(() => {
     console.log(`db is connect at ${connect.connection.host}`);
-  })
-  .catch((err) => console.log("someting wrong", err.message));
+  } catch (err) {
+    console.log(err.message, "data base cant connect");
+    process.exit(1);
+  }
+};
+connecetToDb()
+// mongoose.set("strictQuery", false);
+// //*test
 
-app.get("/", (req, res) => {
-  return res.status(200).send("add connect to db");
-});
-// routes
-// app.use("/cars", carRoute);
-// app.use(page404);
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
