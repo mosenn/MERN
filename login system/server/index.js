43e5c5bb2f43b2696054ba9ccec55c97c5b4dc36
username <mohseen_abasi@yahoo.com>
2023-05-21 13:34:55 +0330

    add connect to db

--- login system/server/index.js
+++ login system/server/index.js
@@ -3,42 +3,30 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 // const session = require("express-session");
-// const connecetToDb = require("./connection/db");
+
+const connecetToDb = require("./connection/db");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-// app.use(bodyParser.json({ limit: "50kb" }));
-// app.use(bodyParser.urlencoded({ limit: "50kb", extended: true }));
 
 app.use(bodyParser.json());
 // cors
 app.use(cors());
 // const carRoute = require("./router/car");
 // const page404 = require("./router/404");
+
+//*data base connection
+connecetToDb();
+
 app.get("/", (req, res) => {
-  return res.status(200).send(" limit 10mb");
+  return res.status(200).send("add connect to db");
 });
 // routes
 // app.use("/cars", carRoute);
 // app.use(page404);
 
-// mongoose.set("strictQuery", false);
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://mnazgul:qse8313656@cluster0.zutazhf.mongodb.net/carDatabase"
-//   )
-//   .then(() => {
-//     console.log("data base is conect");
-//   })
-//   .catch((err) => console.log("someting wrong", err));
 
-// acesse
-// static(app);
-
-// view ejs
-// app.set("view engine", "ejs");
-// app.set("views", "view");
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
