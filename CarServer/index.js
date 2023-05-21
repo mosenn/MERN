668cf947411b581f8a766970f5be2df63e56ccfd
username <mohseen_abasi@yahoo.com>
2023-05-21 11:33:09 +0330

    add teset message for carsv

--- CarServer/index.js
+++ CarServer/index.js
@@ -8,23 +8,25 @@ const app = express();
 app.use(bodyParser.json());
 // cors
 app.use(cors());
-const carRoute = require("./router/car");
-const page404 = require("./router/404");
-
+// const carRoute = require("./router/car");
+// const page404 = require("./router/404");
+app.get("/", (req, res) => {
+  return res.status(200).send("hellow this test for vercel");
+});
 // routes
-app.use("/cars", carRoute);
-app.use(page404);
+// app.use("/cars", carRoute);
+// app.use(page404);
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
-mongoose
-  .connect(
-    "mongodb+srv://mnazgul:qse8313656@cluster0.zutazhf.mongodb.net/carDatabase"
-  )
-  .then(() => {
-    console.log("data base is conect");
-  })
-  .catch((err) => console.log("someting wrong", err));
+// mongoose
+//   .connect(
+//     "mongodb+srv://mnazgul:qse8313656@cluster0.zutazhf.mongodb.net/carDatabase"
+//   )
+//   .then(() => {
+//     console.log("data base is conect");
+//   })
+//   .catch((err) => console.log("someting wrong", err));
 
 // acesse
 static(app);
@@ -32,8 +34,6 @@ static(app);
 // view ejs
 app.set("view engine", "ejs");
 app.set("views", "view");
-
-
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
