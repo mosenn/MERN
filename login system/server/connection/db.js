fc4122b5eeffc804fe448e822efa1f47b1c46004
username <mohseen_abasi@yahoo.com>
2023-05-22 10:32:07 +0330

    remove dotenv in db.js

--- login system/server/connection/db.js
+++ login system/server/connection/db.js
@@ -1,5 +1,5 @@
-const dotEnv = require("dotenv");
-dotEnv.config({ path: "./config.env" });
+// const dotEnv = require("dotenv");
+// dotEnv.config({ path: "./config.env" });
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
