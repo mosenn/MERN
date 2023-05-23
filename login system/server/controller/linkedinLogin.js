7511683935128d6544d069f7e54c3afaf9313c0b
username <mohseen_abasi@yahoo.com>
2023-05-23 06:58:30 +0330

    add linkedinToken_access to userDataLinkedin

--- login system/server/controller/linkedinLogin.js
+++ login system/server/controller/linkedinLogin.js
@@ -11,7 +11,10 @@ const accessTokenLinkedin = async (req, res, next) => {
     console.log("CODE in accessToken Linkedin Controller:", code);
     const likedinToken = await getCodeAccessLinkedin(code);
     console.log("likedinToken in Controller>accessTokenLinkedin", likedinToken);
-    console.log("likedinToken.access_token in Controller>accessTokenLinkedin", likedinToken.access_token);
+    console.log(
+      "likedinToken.access_token in Controller>accessTokenLinkedin",
+      likedinToken.access_token
+    );
     return res.status(200).json(likedinToken.access_token);
   } catch (err) {
     console.log(
@@ -26,6 +29,11 @@ const accessTokenLinkedin = async (req, res, next) => {
 const userDataLinkedin = async (req, res, next) => {
   try {
     // //*Todo findbyOne Token in mongodb but need first token for this .
+    console.log(
+      "likedinToken.access_token in Controller>userDataLinkedin",
+      likedinToken.access_token
+    );
+
     //*find token in db same in token is response
     const tokenInMongodb = await linkdinModel.findOne({
       token: likedinToken.token,
@@ -34,13 +42,18 @@ const userDataLinkedin = async (req, res, next) => {
       console.log("true token in mongodb");
     }
     //* if status 200 in api most be have both tokens
-    console.log("linkedin token in userdataLinkeidn func", likedinToken.token);
+    console.log(
+      "linkedin token in userdataLinkeidn func",
+      likedinToken.access_token
+    );
     console.log("TOKEN COMING FROM MONGODB", tokenInMongodb.token);
     //* if status 200 in api most be have both tokens
     // console.log(tokenInMongodb, "tokenInMongodb");
     //* pass mongodb token to this function for get user data
     // const Userdata = await getTokenAccessUserDataLinkedin(tokenInMongodb.token);
-    const Userdata = await getTokenAccessUserDataLinkedin(likedinToken.token);
+    const Userdata = await getTokenAccessUserDataLinkedin(
+      likedinToken.access_token
+    );
 
     console.log("USERDATA IN FUNC userDataLinkedin", Userdata);
     // return res.status(200).send(tokenInMongodb);
