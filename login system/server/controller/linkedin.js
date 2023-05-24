845df5c898f74e6ed4aa1067606eda90b681f74b
username <mohseen_abasi@yahoo.com>
2023-05-24 17:41:40 +0330

    handel error for login

--- login system/server/controller/linkedin.js
+++ login system/server/controller/linkedin.js
@@ -6,6 +6,8 @@ const {
 const linkdinModel = require("../model/linkedin");
 
 let likedinToken;
+
+//*take code from client pass to api for take token
 const accessTokenLinkedin = async (req, res, next) => {
   try {
     const code = req.query.code;
@@ -51,10 +53,13 @@ const loginLikedin = async (req, res) => {
     if (userDataLikedin) {
       return res.status(200).send(userDataLikedin);
     }
+    if (!userDataLikedin) {
+      throw "you are not log in with this email";
+    }
   } catch (err) {
     console.log("Error from loginLinkedin", err);
     console.log(err.message, "err message for loginlindin controller");
-    return res.status(404).send(err);
+    return res.status(404).send([err.message ,"you are not log in with this email"]);
   }
 };
 module.exports = { accessTokenLinkedin, /*userDataLinkedin*/ loginLikedin };
