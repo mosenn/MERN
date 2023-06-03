5307ec9049da54aaa328210f7b90837f8990b3ce
username <mohseen_abasi@yahoo.com>
2023-06-03 20:42:25 +0330

    create mvc and add animation route

--- movie server/index.js
+++ movie server/index.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 require("dotenv").config({ path: "./config.env" });
 
 const connectingToDatabase = require("./connection/db");
-const movieModel = require("./model/movie");
 
 //* connect to data base
 connectingToDatabase();
 const port = process.env.PORT || 3000;
+
+//*routes
+app.use("/", require("./routes/movies"));
+
 
 //*test route
 app.get("/", (req, res) => {
   return res.send("this is movie server");
 });
 //*movie route
-app.get("/movies", async (req, res) => {
-  const movies = await movieModel.find();
-  return res.status(200).send(movies);
-});
 
 app.listen(port, () => {
   console.log(`server is connect ${port}`);
---
+++ movie server/model/topTenSeris.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const topTenSeris = new mongoose.Schema({
+    
+});
