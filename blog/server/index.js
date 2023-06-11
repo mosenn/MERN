49d3c2fd532e5c6de2294c4d30568de35bd36cd7
username <mohseen_abasi@yahoo.com>
2023-06-11 07:11:43 +0330

    init server and clinet

---
+++ blog/server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require('dotenv').config({path:'./.env'})
+const app = express();
+//*parser
+app.use(bodyParser.json());
+app.use(cookieParser())
+//*cors
+app.use(
+  cors({
+    credentials: true,
+    origin: "",
+  })
+);
+
+//*testing route
+app.use("/", async (req, res) => {
+  res.status(200).send("server is ok");
+});
+
+//*listen
+const port = process.env.PORT || 3010;
+app.listen(port, () => {
+  console.log(`server is connectin http://localhost:${port}`);
+});
