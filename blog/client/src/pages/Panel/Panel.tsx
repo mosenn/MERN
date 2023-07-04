217ae6da670f3521f3319dd805761d9a4328f4ff
username <mohseen_abasi@yahoo.com>
2023-07-04 12:37:41 +0330

    desgin panel

--- blog/client/src/pages/Panel/Panel.tsx
+++ blog/client/src/pages/Panel/Panel.tsx
@@ -32,25 +32,62 @@ const Panel = () => {
       {postUser.length > 0 &&
         postUser.map((post: userPost) => {
           return (
-            <div key={post._id}>
-              <p>{post.title}</p>
-              <figure>
-                <img
-                  src={post.cover}
-                  alt={post.title}
-                  className="w-[50px] h-[50px] rounded-full"
-                />
-              </figure>
-              <p>{format(new Date(post.createdAt), "yyyy-MM-dd")}</p>
-              <button
-                type="button"
-                onClick={() => {
-                  deletePostUser(post._id);
-                }}
-              >
-                delete
-              </button>
-            </div>
+            <section className=" flex flex-col justify-center items-center   w-[100%]">
+              <div key={post._id} className="border w-[100%] md:w-[80%] mt-3 ">
+                <section>
+                  <div className="flex justify-around  items-center p-2 m-3 md:relative">
+                    <figure className="md:w-[15%] flex justify-center">
+                      <img
+                        src={post.cover}
+                        alt={post.title}
+                        className="w-[50px] h-[50px] rounded-full"
+                      />
+                    </figure>
+                    <p className="md:w-[15%] text-center">{post.title}</p>
+
+                    <p className="md:w-[15%] text-center">
+                      {format(new Date(post.createdAt), "yyyy-MM-dd")}
+                    </p>
+                    {/* Buttons in tablet scope */}
+                    <div className="  hidden md:flex  justify-evenly ml-[50px] w-[fit-content]   items-center ">
+                      <button
+                        className="font-semibold m-3 w-[70px] h-[25px] flex justify-center items-center  bg-red-300 p-3 rounded-[5px]"
+                        type="button"
+                        onClick={() => {
+                          deletePostUser(post._id);
+                        }}
+                      >
+                        delete
+                      </button>
+                      <button className="m-3  font-semibold bg-green-300 p-3 rounded-[5px] w-[70px] h-[25px] flex justify-center items-center">
+                        edit
+                      </button>
+                      <button className="m-3 font-semibold bg-blue-300 p-3 rounded-[5px] w-[70px] h-[25px] flex justify-center items-center">
+                        priview
+                      </button>
+                    </div>
+                  </div>
+                  {/* Buttons in mobile scope */}
+                  <div className="flex md:hidden justify-around items-center p-2 m-3">
+                    <button
+                      className="font-semibold m-3 w-[70px] h-[25px] flex justify-center items-center  bg-red-300 p-3 rounded-[5px]"
+                      type="button"
+                      onClick={() => {
+                        deletePostUser(post._id);
+                      }}
+                    >
+                      delete
+                    </button>
+                    <button className="m-3  font-semibold bg-green-300 p-3 rounded-[5px] w-[70px] h-[25px] flex justify-center items-center">
+                      edit
+                    </button>
+                    <button className="m-3 font-semibold bg-blue-300 p-3 rounded-[5px] w-[70px] h-[25px] flex justify-center items-center">
+                      preview
+                    </button>
+                  </div>
+                </section>
+              </div>
+            </section>
           );
         })}
     </div>
